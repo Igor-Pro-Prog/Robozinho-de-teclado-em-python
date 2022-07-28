64e8ffc4aa459cf57d259e2852cf039ae7d483af
Igor Gabriel <igor.gabriel@aluno.ufca.edu.br>
2022-07-28 08:56:58 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Robozinho-de-teclado-em-python
---
+++ Robozinho.py
@@ -0,0 +1,21 @@
+import pyautogui  
+#Automação do teclado
+
+#Aperta alt para baixo
+
+pyautogui.keyDown('alt')
+
+#Pressiona o TAB
+
+pyautogui.press (['tab'])
+
+#Solta a tecla Alt  
+
+pyautogui.keyUp('alt')
+
+#Digita o texto desejado 
+
+pyautogui.write(' Olá Sou uma automatização desenvolvida pelo Python :) Estou te Visualizando')
+
+
+#Teste de automação de teclado para o desenvolvimento de atividades relacionadas a automatização de sistemas
